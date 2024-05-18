aed5cdfaa6255c90a8c64a417ac48998fa05b218
kapilkashyap <kapil.kashyaps@gmail.com>
2024-05-19 01:20:49 +0530

    Passing in configuration while transmuting a JSON from UI

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import store from './data/store.js';
 import { memorySizeOf, transmute } from './dist/transmute.js';
 
 (() => {
+    console.log(
+        '%cTransmute %c#1.0.0\nDynamically transform a JSON into a Class with private properties and accessor methods at runtime.',
+        'font-size:36px;letter-spacing:.15em;',
+        ''
+    );
+
     const beautifyJSON = () => {
         const jsonInput = document.querySelector('.jsonInput').value;
         if (jsonInput != null && jsonInput.length > 0) {
@@ -19,18 +25,20 @@ import { memorySizeOf, transmute } from './dist/transmute.js';
         document.querySelector('.jsonInput').value = JSON.stringify(store, undefined, 4);
     });
 
-    // document.querySelector('.jsonInput').addEventListener('keyup', beautifyJSON);
-
     document.querySelector('button#formatJSON').addEventListener('click', beautifyJSON);
 
     document.querySelector('button#transmuteJSON').addEventListener('click', () => {
         const jsonText = document.querySelector('.jsonInput').value;
         if (jsonText != null && jsonText.length > 0) {
+            const readOnly = document.querySelector('#is-read-only').checked;
+            const deep = document.querySelector('#is-deep').checked;
             console.time('Time to transmute');
-            window['obj'] = transmute(JSON.parse(jsonText));
+            const transmutedObject = transmute(JSON.parse(jsonText), { readOnly, deep });
             console.timeEnd('Time to transmute');
-            console.log('Memory size: ', memorySizeOf(window['obj']));
-            console.log('Created a constant named - obj');
+            window['obj'] = transmutedObject;
+            console.log('Memory size:', memorySizeOf(window['obj']));
+            console.log('Transmuted JSON stored in variable obj > console.log(obj)');
+            console.log('Configuration', { readOnly, deep });
             console.log(window['obj']);
         }
     });
