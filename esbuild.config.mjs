642f1780336ee7ea1dd8eb683f3987b62eddecc0
kapilkashyap <kapil.kashyaps@gmail.com>
2024-05-20 16:11:11 +0530

    Adding a custom esbuild bundler extension that tries to emulate bundling of tsup bundler

---
+++ esbuild.config.mjs
@@ -0,0 +1,49 @@
+/**
+ * Currently I am behind a VPN and we do not have a tsup bundler in our npm registry
+ * 
+ * Caution: This script tries to emulate tsup bundler configurations and it might be removed in future
+ */
+
+import * as esbuild from 'esbuild';
+import { parseArgs } from 'node:util';
+
+const args = process.argv.slice(2);
+// command line options
+const options = {
+    minify: {
+        type: 'string',
+        short: 'm',
+    },
+    format: {
+        type: 'string',
+        short: 'f',
+    },
+    globalName: {
+        type: 'string',
+        short: 'g',
+    }
+};
+
+const { values: { format, minify, globalName } } = parseArgs({ args, options });
+const formats = format == null ? ['cjs'] : format.split(',');
+const namespace = globalName || 'lib.api';
+
+console.log(`Namespace: ${namespace}`);
+
+for (let index = 0; index < formats.length; index++) {
+    const format = formats[index];
+    const outExtensionValue = format === 'esm' ? '.mjs' : format === 'iife' ? '.global.js' : '.js';
+    console.log(`Building format: ${format}`);
+
+    await esbuild.build({
+        entryPoints: ['./src/transmute.ts'],
+        format,
+        bundle: true,
+        minify: !!minify,
+        splitting: false,
+        sourcemap: true,
+        outdir: './dist',
+        outExtension: { '.js': outExtensionValue },
+        globalName: namespace
+    });
+}
