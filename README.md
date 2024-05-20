185c51fba2ef193533a8c8d63a949dc07e58b05f
kapilkashyap <kapil.kashyaps@gmail.com>
2024-05-20 17:55:23 +0000

    Deploying to gh-pages from @ kapilkashyap/transmute@47997d12209f417e476ee3a9f082847e9344efb9 🚀

---
+++ README.md
@@ -0,0 +1,2 @@
+# transmute
+Dynamically transform a JSON into a Class with private properties and accessor methods at runtime.
---
+++ index.js
@@ -0,0 +1,47 @@
+import store from './data/store.js';
+import { memorySizeOf, transmute } from './dist/transmute.mjs';
+
+(() => {
+    console.log(
+        '%cTransmute %c#1.0.0\nDynamically transform a JSON into a Class with private properties and accessor methods at runtime.',
+        'font-size:36px;letter-spacing:.15em;',
+        ''
+    );
+
+    const beautifyJSON = () => {
+        const jsonInput = document.querySelector('.jsonInput').value;
+        if (jsonInput != null && jsonInput.length > 0) {
+            // eslint-disable-next-line no-useless-catch
+            try {
+                const parsedJson = JSON.parse(jsonInput);
+                document.querySelector('.jsonInput').value = JSON.stringify(parsedJson, undefined, 4);
+            } catch (e) {
+                throw e;
+            }
+        }
+    };
+
+    window.addEventListener('load', () => {
+        document.querySelector('.jsonInput').value = JSON.stringify(store, undefined, 4);
+    });
+
+    document.querySelector('button#formatJSON').addEventListener('click', beautifyJSON);
+
+    document.querySelector('button#transmuteJSON').addEventListener('click', () => {
+        const jsonText = document.querySelector('.jsonInput').value;
+        if (jsonText != null && jsonText.length > 0) {
+            const readOnly = document.querySelector('#is-read-only').checked;
+            const deep = document.querySelector('#is-deep').checked;
+
+            console.time('Time to transmute');
+            const transmutedObject = transmute(JSON.parse(jsonText), { readOnly, deep });
+            console.timeEnd('Time to transmute');
+
+            window['obj'] = transmutedObject;
+            console.log('Memory size:', memorySizeOf(window['obj']));
+            console.log('Transmuted JSON stored in variable obj > console.log(obj)');
+            console.log('Configuration', { readOnly, deep });
+            console.log(window['obj']);
+        }
+    });
+})();
